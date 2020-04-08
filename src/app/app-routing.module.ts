0c7df3ce96d62730a93a3e7af223ab99c1232c9b
Michael Blas <michaelblas@Michaels-MacBook-Pro.local>
2020-04-08 01:05:40 -0700

    changes on header, menu addition and home page assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,14 @@ import { ReservationComponent } from './reservation/reservation.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
-  { path: 'about', component: AboutUsComponent },
+  // { path: 'about', component: AboutUsComponent },
   { path: 'contact', component: ContactComponent},
   { path: 'reservation', component: ReservationComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,16 +9,40 @@ import { HttpClient } from '@angular/common/http';
 export class MenuComponent implements OnInit {
 
   dishes;
+  moreOrLessBuffet = false;
+  moreOrLessCatering = false;
   constructor(
+    // private router: Router,
     private httpClient: HttpClient
   ) { }
 
+  /**
+   * @todo Enable download PDF
+   * @todo Add language support
+   * @todo Apply font for styles
+   */
+
   ngOnInit() {
+
+    // this.router.events.subscribe((evt) => {
+    //   if (!(evt instanceof NavigationEnd)) {
+    //     return;
+    //   }
+    //   window.scrollTo(0, 0)
+    // });
+
     this.httpClient.get('assets/menu.json').subscribe(res => {
       console.log(res);
       this.dishes = res;
-      console.log(this.dishes.categories[0].category_1)
     });
+  }
+
+  changeTextBuffetBtn() {
+    this.moreOrLessBuffet = !this.moreOrLessBuffet;
+  }
+
+  changeTextCateringBtn() {
+    this.moreOrLessCatering = !this.moreOrLessCatering;
   }
 
 }
